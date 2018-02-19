a40ef4115cb275d609a5e207f3f135e061a007d6
Mikaël Cluseau <mcluseau@isi.nc>
2018-02-19 14:32:33 +1100

    feat(image2pos): initial commit

---
+++ example/image2pos/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"image"
+	"log"
+	"os"
+
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+
+	"github.com/ISI-nc/escpos"
+	"github.com/ISI-nc/escpos/raster"
+)
+
+var (
+	lpDev     = flag.String("p", "/dev/usb/lp0", "Printer dev file")
+	imgPath   = flag.String("i", "image.png", "Input image")
+	threshold = flag.Float64("t", 0.5, "Black/white threshold")
+	align     = flag.String("a", "center", "Alignment (left, center, right)")
+	doCut     = flag.Bool("c", false, "Cut after print")
+	maxWidth  = flag.Int("printer-max-width", 512, "Printer max width in pixels")
+)
+
+func main() {
+	flag.Parse()
+
+	imgFile, err := os.Open(*imgPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	img, imgFormat, err := image.Decode(imgFile)
+	imgFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Loaded image, format: ", imgFormat)
+
+	// ----------------------------------------------------------------------
+
+	f, err := os.OpenFile(*lpDev, os.O_WRONLY, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+	log.Print(*lpDev, " open.")
+
+	ep := escpos.New(f)
+
+	ep.Init()
+
+	ep.SetAlign(*align)
+
+	rasterConv := &raster.Converter{
+		MaxWidth:  *maxWidth,
+		Threshold: *threshold,
+	}
+
+	rasterConv.Print(img, ep)
+
+	if *doCut {
+		ep.Cut()
+	}
+	ep.End()
+}
